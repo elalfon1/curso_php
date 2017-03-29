ae1c81e85b21dbce48757af7427aebd951619418
DESKTOP-JHVS5UR\Alfonso <esesito@gmail.com>
2017-03-29 14:39:14 +0200

    BBDD - Control Errores y Bucle de ResultSet

--- 33-46_BBDD/conexion_BBDD.php
+++ 33-46_BBDD/conexion_BBDD.php
@@ -15,7 +15,15 @@
 			
 			//´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´POR PROCEDIMIENTOS´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´\\
 			
-			$conexion = mysqli_connect($dbHost, $dbUsuario, $dbContra, $dbNombre);
+			$conexion = mysqli_connect($dbHost, $dbUsuario, $dbContra); //Nos conectamos al SERVIDOR de BBDD
+			
+			
+			if(mysqli_connect_errno()){ //Controlamos el error de conexion al servidor
+				echo "Fallo al conectar con el servidor de BBDD";
+				exit();
+			}
+			
+			mysqli_select_db($conexion, $dbNombre) or die("No se encuentra la BBDD"); //Nos conectamos a BBDD especifica
 			
 			mysqli_set_charset($conexion, "utf8"); //Para arreglar lo de las tildes de los huevos
 			
@@ -26,12 +34,17 @@
 			
 			$resultados = mysqli_query($conexion, $consulta);
 			
-			$fila = mysqli_fetch_row($resultados);
 			
-			echo $fila[0]." ";
-			echo $fila[1]." ";
-			echo $fila[2]." ";
-			echo $fila[3]." ";
+			//while( ($fila = mysqli_fetch_row($resultados)) == true ){ //Si hay informacion en el resultset
+			while($fila = mysqli_fetch_row($resultados)){ //Es lo mismo
+				
+				echo $fila[0]." ";
+				echo $fila[1]." ";
+				echo $fila[2]." ";
+				echo $fila[3]." ";
+				echo "<br>";
+			
+			}
 			
 			
 		?>
